35f22e85750d2e5875a62de271156a56746c0911
Alec Jordan <alecbjordan@gmail.com>
2026-07-08 12:58:44 -0400

    Add neon history_retention_seconds setting for free tier.

--- terraform/database.tf
+++ terraform/database.tf
@@ -3,4 +3,7 @@ resource "neon_project" "main" {
   region_id  = "aws-us-east-1"
   pg_version = 17
   org_id     = var.neon_org_id
+
+  # Free tier maximum is 6 hours (21600 seconds)
+  history_retention_seconds = 21600
 }
